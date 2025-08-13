306851d8947d6575f5e792d8ff3fad092e83cf01
Muttu R K <kagalmutturaj@gmail.com>
2025-08-14 05:26:37 +0530

    Update dp.java

--- dp.java
+++ dp.java
@@ -1,12 +1,15 @@
-   public int frogJump(int[] heights) {
-        int []dp=new int[heights.length+1];
-        for(int i=0;i<dp.length;i++)dp[i]=-1;
-        return frogJump(heights,dp,dp.length-1);
+    public static int frogJump(int[] heights) {
+        int[] dp = new int[heights.length + 1];
+        Arrays.fill(dp, -1);
+        return frogJump(heights, dp, dp.length - 2);
     }
-    public int frogJump(int []h,int[]dp,int index){
-        if(index==0) return 0;
-        if(dp[index]!=-1) return dp[index];
-        int left=frogJump(h,dp,index+1)+Math.min(h[index],h[index-1]);
-        int right=frogJump(h,dp,index+2)+Math.max(h[index],h[index-2]);
-        return dp[index]=Math.min(left,right);
+
+    public static int frogJump(int[] h, int[] dp, int index) {
+        if (index <= 0) return 0;
+        if (dp[index] != -1) return dp[index];
+        int left = frogJump(h, dp, index - 1) + Math.abs(h[index] - h[index - 1]);
+        int right = Integer.MAX_VALUE;
+        if (index > 1)
+            right = frogJump(h, dp, index - 2) + Math.abs(h[index] - h[index - 2]);
+        return dp[index] = Math.min(left, right);
     }
