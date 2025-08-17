4c2c65ef9128b8306fabcb64613ce71efc1ab06a
Muttu R K <kagalmutturaj@gmail.com>
2025-08-17 22:43:30 +0530

    Create Unique Paths.java

---
+++ Unique Paths.java
@@ -0,0 +1,11 @@
+int uniquePaths(int m,int n){
+        int [][]dp=new int[m][n];
+        return uniquePaths(m-1,n-1,dp);
+    }
+    public int uniquePaths(int m, int n,int[][]dp) {
+        if (m == 0 || n == 0)
+            return 1;
+            if(dp[m][n]!=0) return dp[m][n];
+            dp[m][n]= uniquePaths(m - 1, n,dp)+uniquePaths(m,n-1,dp);
+            return dp[m][n];
+    }
