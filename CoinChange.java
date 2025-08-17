ce021f6732dc7af893f044ea727943a2323d4b38
Muttu R K <kagalmutturaj@gmail.com>
2025-08-17 22:08:12 +0530

    Create CoinChange.java

---
+++ CoinChange.java
@@ -0,0 +1,13 @@
+  public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = amount + 1;
+            for (int j = 0; j < coins.length; j++) {
+                if (i >= coins[j]) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                }
+            }
+        }
+        return dp[amount] < amount + 1 ? dp[amount] : -1;
+    }
