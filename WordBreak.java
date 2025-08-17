0b86ff84d23a6736f15d027e199731ce2ec42b37
Muttu R K <kagalmutturaj@gmail.com>
2025-08-17 22:05:25 +0530

    Create WordBreak.java

---
+++ WordBreak.java
@@ -0,0 +1,19 @@
+
+public boolean wordBreak(String s, List<String> wordDict) {
+     boolean []dp=new boolean[s.length()+1];
+     HashSet<String> words=new HashSet<>(wordDict);
+     dp[0]=true;
+     int maxLength=0;
+     for(String s1:wordDict){
+        if(s1.length()>maxLength)maxLength=s1.length();
+     }
+     for(int i=1;i<dp.length;i++){
+        for(int j=i-1;j>=Math.max(0,i-maxLength);j--){
+            if(dp[j]&&words.contains(s.substring(j,i))){
+                dp[i]=true;
+                break;
+            }
+        }
+     }   
+     return dp[s.length()];
+    }
