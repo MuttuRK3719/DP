47e4e8521816c18984acfeef952f7f551133cd5c
Muttu R K <kagalmutturaj@gmail.com>
2025-08-17 22:08:32 +0530

    Update CoinChange.java

--- CoinChange.java
+++ CoinChange.java
@@ -1,4 +1,6 @@
-  public int coinChange(int[] coins, int amount) {
+https://leetcode.com/problems/coin-change/
+
+public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
         dp[0] = 0;
         for (int i = 1; i < dp.length; i++) {
