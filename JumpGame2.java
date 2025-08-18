c9ad0ca5d1761657440968f1b0d8c05ec3ad83df
Muttu R K <kagalmutturaj@gmail.com>
2025-08-18 23:34:04 +0530

    Update JumpGame2.java

--- JumpGame2.java
+++ JumpGame2.java
@@ -1,4 +1,12 @@
 
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Input: nums = [2,3,0,1,4]
+Output: 2
+
+        
 public static  int jump(int[] nums) {
         int currentIndex = 0, currentMax = -1, jump = 0;
         for (int i = 0; i < nums.length - 1; i++) {
