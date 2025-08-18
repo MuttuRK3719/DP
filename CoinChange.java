c71f1e3d4239ae77863e0e32f37b9c202fbce3b6
Muttu R K <kagalmutturaj@gmail.com>
2025-08-18 10:58:58 +0530

    Update CoinChange.java

--- CoinChange.java
+++ CoinChange.java
@@ -1,4 +1,17 @@
 https://leetcode.com/problems/coin-change/
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
 
 public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
