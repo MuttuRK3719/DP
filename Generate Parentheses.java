dad650961981a38832d2d5186723562c1cee0ee2
Muttu R K <kagalmutturaj@gmail.com>
2025-08-18 19:59:13 +0530

    Update Generate Parentheses.java

--- Generate Parentheses.java
+++ Generate Parentheses.java
@@ -1,21 +1,28 @@
- public List<String> generateParenthesis(int n) {
-        List<String>ans=new ArrayList<>();
-        backtracking(ans,new StringBuilder(),0,0,n);
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Input: n = 1
+Output: ["()"]
+
+   public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        backtracking(ans, new StringBuilder(), 0, 0, n);
         return ans;
     }
-    void backtracking(List<String>result,StringBuilder current,int open,int close,int max){
-        if(current.length()==2*max){
+
+    void backtracking(List<String> result, StringBuilder current, int open, int close, int max) {
+        if (current.length() == 2 * max) {
             result.add(current.toString());
             return;
         }
-        if(open<max){
+        if (open < max) {
             current.append("(");
-            backtracking(result,current,open+1,close,max);
-            current.deleteCharAt(current.length()-1);
+            backtracking(result, current, open + 1, close, max);
+            current.deleteCharAt(current.length() - 1);
         }
-        if(close<open){
+        if (close < open) {
             current.append(")");
-            backtracking(result,current,open,close+1,max);
-            current.deleteCharAt(current.length()-1);
+            backtracking(result, current, open, close + 1, max);
+            current.deleteCharAt(current.length() - 1);
         }
     }
