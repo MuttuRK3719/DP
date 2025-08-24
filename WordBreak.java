afb0d06eb1892a86c1f57f567099841d561761d2
Muttu R K <kagalmutturaj@gmail.com>
2025-08-24 16:58:23 +0530

    Update WordBreak.java

--- WordBreak.java
+++ WordBreak.java
@@ -1,4 +1,21 @@
 https://leetcode.com/problems/word-break/
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+
+     
 public boolean wordBreak(String s, List<String> wordDict) {
      boolean []dp=new boolean[s.length()+1];
      HashSet<String> words=new HashSet<>(wordDict);
