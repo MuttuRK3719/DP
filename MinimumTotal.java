d3bd0313d2320817e27f0b7ae793e0719ec59e36
Muttu R K <kagalmutturaj@gmail.com>
2025-09-01 22:32:51 +0530

    Update MinimumTotal.java

--- MinimumTotal.java
+++ MinimumTotal.java
@@ -28,3 +28,21 @@ public int minimumTotal(List<List<Integer>> triangle) {
         }
         return dp[0];
     }
+
+public int minimumTotal(List<List<Integer>> triangle) {
+        int[][] dp = new int[triangle.size()][triangle.size() - 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+        return findMinPath(triangle, 0, 0, dp);
+    }
+
+int findMinPath(List<List<Integer>> triangle, int i, int j, int[][] dp) {
+        if (i == triangle.size() - 1)
+            return triangle.get(i).get(j);
+        if (dp[i][j] != Integer.MIN_VALUE)
+            return dp[i][j];
+        int left = findMinPath(triangle, i + 1, j, dp);
+        int right = findMinPath(triangle, i + 1, j + 1, dp);
+        return dp[i][j] = triangle.get(i).get(j) + Math.min(left, right);
+    }
