efdc198a700f9d806096c8cd25f9dfb37b8773d6
Muttu R K <kagalmutturaj@gmail.com>
2025-11-06 16:03:39 +0530

    Update Unique Paths With Obstacles.java

--- Unique Paths With Obstacles.java
+++ Unique Paths With Obstacles.java
@@ -32,3 +32,30 @@ int f(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp) {
         //  return f(m-1,n-1,obstacleGrid,dp);
     }
 
+ int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        if(obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1) return 0;
+        vector<int>p(n);
+        for(int i=0;i<m;i++){
+            vector<int> temp(n);
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0){
+                    temp[j]=1;
+                    continue;
+                }
+                if(obstacleGrid[i][j]==1){
+                    temp[j]=0;
+                    continue;
+                }
+                int down=0,left=0;
+                if(i>0)down=p[j];
+                if(j>0)left=temp[j-1];
+
+                temp[j]=left+down;
+            }
+            p=temp;
+        }
+        return p[n-1];
+
+    }
