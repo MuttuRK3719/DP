acac15deb548671acb3b09dc7981bd27d219695b
Muttu R K <kagalmutturaj@gmail.com>
2025-11-06 22:50:52 +0530

    Update MinFallingPathSum.java

--- MinFallingPathSum.java
+++ MinFallingPathSum.java
@@ -17,3 +17,22 @@
         int ans=min(leftDown,min(rightDown,down));
         return dp[row][col]=ans;
     }
+
+ int minFallingPathSum(vector<vector<int>>& matrix) {
+        int m=matrix.size(),n=matrix[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+    dp=matrix;
+    for(int i=m-2;i>=0;i--){
+        for(int j=0;j<n;j++){
+            int down=dp[i+1][j];
+            int leftDown=(j>0)?dp[i+1][j-1]:1e8;
+            int rightDown=(j<n-1)?dp[i+1][j+1]:1e8;
+            dp[i][j]+=min({down,leftDown,rightDown});
+        }
+    }
+    int ans=1e9;
+    for(int j=0;j<n;j++){
+        ans=min(ans,dp[0][j]);
+    }
+    return ans;
+           }
