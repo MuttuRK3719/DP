e0b44c22abca01775d8c69c2c0b786b6a0bc97a9
Muttu R K <kagalmutturaj@gmail.com>
2025-11-11 21:20:12 +0530

    Update LCSubString.cpp

--- LCSubString.cpp
+++ LCSubString.cpp
@@ -18,3 +18,27 @@ int LCSubStr(string &str1, string &str2)
     // Write your code here
 
 }
+
+
+int LCSubStr(string &str1, string &str2)
+{
+    int n=str1.size(),m=str2.size();
+
+    vector<int>prev(m+1);
+    int ans=0;
+    for(int i=1;i<=n;i++){
+        vector<int>curr(m+1);
+        for(int j=1;j<=m;j++){
+            if(str1[i-1]==str2[j-1]){
+                curr[j]=1+prev[j-1];
+                ans=max(ans,curr[j]);
+            }
+            else curr[j]=0;
+        }
+        prev=curr;
+    }
+
+    return ans;
+    // Write your code here
+
+}
