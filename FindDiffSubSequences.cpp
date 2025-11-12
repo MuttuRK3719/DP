bce93a961001a1b4262331e20d938e43524bde9b
Muttu R K <kagalmutturaj@gmail.com>
2025-11-12 15:38:18 +0530

    Update FindDiffSubSequences.cpp

--- FindDiffSubSequences.cpp
+++ FindDiffSubSequences.cpp
@@ -16,3 +16,20 @@ int numDistinct(string s, string t) {
         else
             return dp[m][n] = findRes(s, t, m - 1, n, dp);
     }
+
+
+int numDistinct(string s, string t) {
+        int m = s.size(), n = t.size();
+        vector<vector<double >> dp(m+1, vector<double>(n+1));
+        for(int j=0;j<=m;j++){
+            dp[j][0]=1;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==t[j-1])dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return (int)dp[m][n];
+        // return findRes(s, t, m - 1, n - 1, dp);
+    }
