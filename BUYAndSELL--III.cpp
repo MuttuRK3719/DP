0ee59e83f7fd6d4d205c3a4aa05592bfe76cacde
Muttu R K <kagalmutturaj@gmail.com>
2025-11-13 10:29:13 +0530

    Implement dynamic programming for max profit calculation

--- BUYAndSELL--III.cpp
+++ BUYAndSELL--III.cpp
@@ -15,3 +15,24 @@
         }
         return dp[index][buy][cap]=profit;
     }
+
+int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        int profit=0;
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<=2;cap++){
+                    if(buy){
+                        profit=max(dp[i+1][buy][cap],-prices[i]+dp[i+1][0][cap]);
+                    }
+                    else {
+                        profit=max(dp[i+1][buy][cap],prices[i]+dp[i+1][1][cap-1]);
+                    }
+                    dp[i][buy][cap]=profit;
+                }
+            }
+        }
+        return dp[0][1][2];
+        // return getProfit(0,prices,dp,1,2);
+    }
