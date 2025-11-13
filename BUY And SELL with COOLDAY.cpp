d8bf3fdd2c3540c99d1888feaac9bcd0f83d4938
Muttu R K <kagalmutturaj@gmail.com>
2025-11-13 13:26:15 +0530

    Update BUY And SELL with COOLDAY.cpp

--- BUY And SELL with COOLDAY.cpp
+++ BUY And SELL with COOLDAY.cpp
@@ -22,3 +22,22 @@ public:
         return dp[index][buy] = profit;
     }
 };
+
+
+int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+                if (buy == 0) {
+                    profit = max(dp[i + 1][buy], -prices[i] + dp[i + 1][1]);
+                } else {
+                    profit = max(dp[i + 1][buy], prices[i] + dp[i + 2][0]);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][0];
+        // return getMax(prices, 0, dp, 0);
+    }
