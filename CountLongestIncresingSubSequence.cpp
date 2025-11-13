923e5285583f267f23d970180e7b598d8096904a
Muttu R K <kagalmutturaj@gmail.com>
2025-11-13 20:18:46 +0530

    Create CountLongestIncresingSubSequence.cpp

---
+++ CountLongestIncresingSubSequence.cpp
@@ -0,0 +1,22 @@
+int findNumberOfLIS(vector<int>& nums) {
+        int maxi = 0;
+        int n = nums.size();
+        vector<int> dp(n, 1), count(n, 1);
+        for (int i = 0; i < n; i++) {
+            for (int prev = 0; prev < i; prev++) {
+                if (nums[prev] < nums[i] && dp[prev] + 1 > dp[i]) {
+                    dp[i] = dp[prev] + 1;
+                    count[i] = count[prev];
+                } else if (nums[prev] < nums[i] && dp[prev] + 1 == dp[i]) {
+                    count[i] += count[prev];
+                }
+            }
+            maxi = max(maxi, dp[i]);
+        }
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == maxi)
+                c += count[i];
+        }
+        return c;
+    }
