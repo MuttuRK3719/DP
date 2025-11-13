ee36b437862a51bba6e0d4200e9044b3414f1596
Muttu R K <kagalmutturaj@gmail.com>
2025-11-13 12:45:40 +0530

    Update BUY And SELL  with K.cpp

--- BUY And SELL  with K.cpp
+++ BUY And SELL  with K.cpp
@@ -21,3 +21,22 @@ int getMax(vector<int>& prices, int index, int k, int trans,
         vector<vector<int>> dp(n, vector<int>(2*k, -1));
         return getMax(prices, 0,2*k, 0, dp);
     }
+
+int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2*k+1, 0));
+        for(int i=n-1;i>=0;i--){
+            int profit=0;
+            for(int tran=2*k-1;tran>=0;tran--){
+                if(tran%2==0){
+                    profit=max(dp[i+1][tran],-prices[i]+dp[i+1][tran+1]);
+                }
+                else {
+                    profit=max(dp[i+1][tran],prices[i]+dp[i+1][tran+1]);
+                }
+                dp[i][tran]=profit;
+            }
+        }
+        return dp[0][0];
+        // return getMax(prices, 0,2*k, 0, dp);
+    }
