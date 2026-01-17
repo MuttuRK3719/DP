f802bde7bece462e26ecc75330760c17abcd0e58
Muttu R K <kagalmutturaj@gmail.com>
2026-01-17 16:04:36 +0530

    Update Greedy-minPlatform.java

--- src/precode/Greedy-minPlatform.java
+++ src/precode/Greedy-minPlatform.java
@@ -1,4 +1,8 @@
-  public int minPlatform(int arr[], int dep[]) {
+[900, 940, 950, 1100, 1500, 1800], dep[] = [910, 1200, 1120, 1130, 1900, 2000]
+Output: 3
+Explanation: There are three trains during the time 9:40 to 12:00. So we need a minimum of 3 platforms.
+
+public int minPlatform(int arr[], int dep[]) {
         //  code here
         Arrays.sort(arr);
         Arrays.sort(dep);
